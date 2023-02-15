df95f2e912c08766520d465b7a68c11b8f8e4060
Filip Płonka <filip@1f389.com>
2023-02-15 19:03:31 +0100

    Add basic pause menu user interface functionality

--- go.mod
+++ go.mod
@@ -2,11 +2,7 @@ module go-llca
 
 go 1.20
 
-require (
-	github.com/hajimehoshi/ebiten v1.12.12
-	github.com/hajimehoshi/ebiten/v2 v2.4.16
-	golang.org/x/image v0.5.0
-)
+require github.com/hajimehoshi/ebiten/v2 v2.4.16
 
 require (
 	github.com/ebitengine/purego v0.1.1 // indirect
@@ -14,7 +10,7 @@ require (
 	github.com/hajimehoshi/file2byteslice v1.0.0 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
 	golang.org/x/exp/shiny v0.0.0-20230213192124-5e25df0256eb // indirect
+	golang.org/x/image v0.5.0 // indirect
 	golang.org/x/mobile v0.0.0-20221110043201-43a038452099 // indirect
 	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"image/color"
-	"image/png"
 	"log"
 	"math/rand"
 	"os"
+	"sort"
 	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -16,41 +16,18 @@ import (
 
 // cool ones: 2456/5678, 3456/5678, 456/1234, 45678/2345, 345/4567 @156-165, 36/125, 3/123456, 3/1347
 var (
-	BRules    = [...]uint8{3}
-	SRules    = [...]uint8{2, 3}
-	runNumber int
-)
-
-const (
+	BRules                                = []uint8{3}
+	SRules                                = []uint8{2, 3}
 	scaleFactor                           = 2
 	avgStartingLiveCellPercentage float64 = 6.0 // # out of 100
-	saveRun                               = false
 	gensToRun                             = 100 * 60
+	seed                          int64   = 0
 )
 
+const ()
+
 func init() {
-	rand.New(rand.NewSource(0))
-
-	if saveRun {
-		path := "run_count"
-		dat, err := os.ReadFile(path)
-		if err != nil {
-			panic(err)
-		}
-		runNumber, _ = strconv.Atoi(string(dat))
-		runNumber++
-		fmt.Printf("run number: %v\n", runNumber)
-
-		err = os.WriteFile(path, []byte(strconv.Itoa(runNumber)), 0o666)
-		if err != nil {
-			panic(err)
-		}
-
-		err = os.Mkdir(fmt.Sprintf("img/%v", runNumber), 0o777)
-		if err != nil {
-			panic(err)
-		}
-	}
+	rand.New(rand.NewSource(seed))
 }
 
 func becomesAlive(n uint8) bool {
@@ -77,6 +54,13 @@ func becomesDead(n uint8) bool {
 	return true
 }
 
+type EditState bool
+
+const (
+	ChangingBirthRules   EditState = false
+	ChangingSurviveRules EditState = true
+)
+
 type Game struct {
 	worldGrid    []uint8
 	gridX, gridY int
@@ -84,25 +68,26 @@ type Game struct {
 	generation   int
 	name         string
 	isPaused     bool
+	editState    EditState
 }
 
 func (g *Game) Update() error {
 	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
 		g.isPaused = !g.isPaused
 	}
-	if g.isPaused {
-		return nil
+	if inpututil.IsKeyJustPressed(ebiten.KeyC) {
+		g.Restart()
 	}
-	if saveRun {
-		// fmt.Printf("%06v\n", runNumber)
-		f, err := os.Create(fmt.Sprintf("img/%v/%v_%06v.png", runNumber, g.name, g.generation))
-		if err != nil {
-			panic(err)
+	if g.isPaused {
+		if inpututil.IsKeyJustPressed(ebiten.KeyB) {
+			g.editState = ChangingBirthRules
+		} else if inpututil.IsKeyJustPressed(ebiten.KeyS) {
+			g.editState = ChangingSurviveRules
 		}
-		defer f.Close()
-		if err = png.Encode(f, g.pixels); err != nil {
-			log.Printf("failed to encode pixels as png")
-		}
+
+		sort.Slice(BRules, func(i, j int) bool { return BRules[i] < BRules[j] })
+		sort.Slice(SRules, func(i, j int) bool { return SRules[i] < SRules[j] })
+		return nil
 	}
 	g.generation++
 	if g.generation == gensToRun {
@@ -142,26 +127,48 @@ func (g *Game) Update() error {
 	return nil
 }
 
+func (g *Game) Restart() {
+	g.Init()
+	g.generation = 0
+}
+
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.DrawImage(g.pixels, nil)
-	// if !saveRun {
-	// 	ebitenutil.DebugPrint(screen, fmt.Sprintf("%.2f gen %v", ebiten.ActualFPS(), g.generation))
-	// }
+	if g.isPaused {
+		transparencyOverlay := ebiten.NewImage(g.gridX, g.gridY)
+		// transparencyOverlay.Fill(color.RGBA64{255, 255, 255, 128})
+		c := color.Black
+		fmt.Println(c.RGBA())
+		transparencyOverlay.Fill(color.RGBA{0, 0, 0, 255 * 3 / 4}) // black but not completely opaque
+		screen.DrawImage(transparencyOverlay, nil)
 
-	info := "birth rules: "
-	for _, v := range BRules {
-		info += strconv.Itoa(int(v)) + " "
+		info := ""
+		info += "use number keys to modify cell "
+		if g.editState == ChangingBirthRules {
+			info += "BIRTH"
+		} else {
+			info += "SURIVAL"
+		}
+		info += " rules (press S or B to switch)\n"
+
+		info += "birth rules: "
+		for _, v := range BRules {
+			info += strconv.Itoa(int(v)) + " "
+
+		}
+		info += "\n"
+		info += "survive rules: "
+		for _, v := range SRules {
+			info += strconv.Itoa(int(v)) + " "
+
+		}
+		info += "\n"
+
+		info += fmt.Sprintf("%.2f gen %v", ebiten.ActualFPS(), g.generation)
+
+		ebitenutil.DebugPrint(screen, info)
 
 	}
-	info += "\n"
-	info += "survive rules: "
-	for _, v := range SRules {
-		info += strconv.Itoa(int(v)) + " "
-
-	}
-	info += "\n"
-
-	ebitenutil.DebugPrint(screen, info)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -221,5 +228,10 @@ func main() {
 // 	- automaton rule
 // 	- scale
 // 	- fps (capped vs uncapped)
+//  - random density
 // 	- seed
 // 	- hide fps / generation bar
+
+// space to pause/unpause
+// enter to start a new run with the given settings
+// or maybe enter to finish editing s/b rules, and then a button to start?
